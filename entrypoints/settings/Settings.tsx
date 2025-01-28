81231c6f541d06855dccf0c749446391e67e1bb7
castella-cake <65476384+castella-cake@users.noreply.github.com>
2025-01-29 01:08:57 +0900

    FIX: 誤ったリポジトリへのリンクを修正

--- entrypoints/settings/Settings.tsx
+++ entrypoints/settings/Settings.tsx
@@ -19,7 +19,7 @@ createRoot(rootElement).render(
                     <div className="title toptitle"><a href="settings.html" target="_blank" rel="noopener noreferrer" className="optlink">MintWatch</a></div>
                     <div className="titlelink-container">
                         <a href="https://discord.com/invite/GNDtKuu5Rb" target="_blank" rel="noopener noreferrer" className="titlelink">Discord</a>
-                        <a href="https://github.com/castella-cake/niconico-peppermint-extension/issues" target="_blank" rel="noopener noreferrer" className="titlelink">Feedback</a>
+                        <a href="https://github.com/castella-cake/mintwatch/issues" target="_blank" rel="noopener noreferrer" className="titlelink">Feedback</a>
                     </div>
                 </div>
                 <div className="settings-page-desc">{lang.SETTINGS_DESC}</div>
@@ -39,7 +39,7 @@ createRoot(rootElement).render(
                         設定は自動保存されます。保存した設定を反映させるには、リロードする必要があります。<br/>
                         MintWatch の更新後に表示が崩れた場合は、Ctrl+Shift+Rでハード再読み込みを試してみてください。
                     </div>
-                    MintWatch <a href="https://github.com/castella-cake/niconico-peppermint-extension" target="_blank" rel="noopener noreferrer">Github Repo</a><br/>
+                    MintWatch <a href="https://github.com/castella-cake/mintwatch" target="_blank" rel="noopener noreferrer">Github Repo</a><br/>
                     Created by CYakigasi <a href="https://www.cyakigasi.net" target="_blank" rel="noopener noreferrer" style={{marginRight: 4}}>Website</a>
                     <a href="https://www.nicovideo.jp/user/92343354" target="_blank" rel="noopener noreferrer" style={{marginRight: 4}}>Niconico</a>
                     <a href="https://discord.com/invite/GNDtKuu5Rb" target="_blank" rel="noopener noreferrer" style={{marginRight: 4}}>Discord</a>
