1fe92a5e462d075a3e2212268f2ec9bc3c131204
castella-cake <65476384+castella-cake@users.noreply.github.com>
2025-01-28 20:15:03 +0900

    FIX: スクリプトの実行阻止が遅い回線で失敗する問題を修正

--- entrypoints/watch.content.ts
+++ entrypoints/watch.content.ts
@@ -48,33 +48,26 @@ export default defineContentScript({
             const localStorage: { [key: string]: any } = storages[1].value;
             //if (!syncStorage.enablewatchpagereplace) return;
 
+            // nopmwだったら終了
             const queryString = location.search;
             const searchParams = new URLSearchParams(queryString);
             if (searchParams.get("nopmw") == "true") return;
 
-            document.documentElement.classList.add(`PMW-Enabled`);
-
-            if ( import.meta.env.FIREFOX ) window.stop() // これでなぜかFirefoxで虚無になる問題が治る。逆にChromeのコードに入れると問題が起こる。
+            // これでなぜかFirefoxで虚無になる問題が治る。逆にChromeのコードに入れると問題が起こる。
+            if ( import.meta.env.FIREFOX ) window.stop() 
+            
             if (!document.documentElement) return;
+
+            // PMW-Enabledを追加してスタイルシートを有効化
+            document.documentElement.classList.add(`PMW-Enabled`);
             //console.log(document.documentElement.outerHTML)
 
             // わたってくるdocumentには既に動画情報のレスポンスが入っている。使えるならこっちを使って高速化してしまったほうが良いので、innerHTMLが書き換わる前に取得しておく
             const initialResponse = (document.getElementsByName('server-response').length > 0 && document.getElementsByName('server-response')[0].getAttribute("content")) ?? "" 
             const initialClassNames = document.documentElement.classList.values();
             const initialStyle = document.documentElement.style.cssText;
-            document.documentElement.querySelectorAll('script').forEach(blockScriptElement);
-
-            document.documentElement.innerHTML = `
-                <head>
-                    <meta charset="utf-8">
-                    <link rel="shortcut icon" href="https://resource.video.nimg.jp/web/images/favicon/favicon.ico">
-                    <meta name="initial-response" content="{}">
-                </head>
-                <body>
-                    <div id="ads-130"></div>
-                </body>
-            `;
-            const head = document.head;
+            
+            // スクリプトの実行を早々に阻止する。innerHTMLの前にやった方が安定する。
             const observer = new MutationObserver((records) => {
                 records.forEach((record) => {
                     const addedNodes = record.addedNodes;
@@ -89,9 +82,22 @@ export default defineContentScript({
                     }
                 });
             });
-            observer.observe(document.head, { childList: true, subtree: true });
+            observer.observe(document.documentElement, { childList: true, subtree: true });
+
+            document.documentElement.innerHTML = `
+                <head>
+                    <meta charset="utf-8">
+                    <link rel="shortcut icon" href="https://resource.video.nimg.jp/web/images/favicon/favicon.ico">
+                    <meta name="initial-response" content="{}">
+                </head>
+                <body>
+                    <div id="ads-130"></div>
+                </body>
+            `;
+            const head = document.head;
             document.dispatchEvent(new CustomEvent("pmw_pageReplaced", { detail: "" }));
 
+            // 外部HLSプラグインを読み込む。pmw-ispluginを入れておかないとスクリプトの実行が阻止されます
             if (import.meta.env.FIREFOX || syncStorage.pmwforcepagehls) {
                 const script = document.createElement("script");
                 script.src = browser.runtime.getURL("/watch_injector.js");
@@ -105,7 +111,7 @@ export default defineContentScript({
             if (document.getElementsByName('server-response').length > 0 && initialResponse) {
                 document.getElementsByName('initial-response')[0].setAttribute("content", initialResponse);
             }
-
+            // PepperMint+ のダークモードが使えるようにここも引き継ぐ
             document.documentElement.classList.add(...initialClassNames);
             document.documentElement.setAttribute(
                 "style",
@@ -113,6 +119,10 @@ export default defineContentScript({
                 ${initialStyle}`
             );
 
+            // watchPage は playersettings の値が何かしら入ってないと落ちるので絶対に書き込む
+            if (!localStorage.playersettings) browser.storage.local.set({ playersettings: {} });
+
+            // root要素を足してレンダー！
             const body = document.body;
             const root = document.createElement("div");
             root.id = "root-pmw";
@@ -121,9 +131,6 @@ export default defineContentScript({
                 console.error("Watch page replace failed: #root is not empty.");
                 return;
             }
-
-            if (!localStorage.playersettings) chrome.storage.local.set({ playersettings: {} });
-
             createRoot(root).render(watchPage());
         }
     }
