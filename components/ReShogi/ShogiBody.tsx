c2c940c129dbb31dc58bfa937bc8d8b5fdab85d4
castella-cake <65476384+castella-cake@users.noreply.github.com>
2025-04-18 20:46:47 +0900

    CHANGE: 一部文字列を変更

--- components/ReShogi/ShogiBody.tsx
+++ components/ReShogi/ShogiBody.tsx
@@ -10,10 +10,11 @@ export default function ShogiBody() {
     return (
         <div className="container reshogi-container">
             <div className="shogi-yorokobi-message">
-                Welcome to the Project Re:Shogi ranking page!!!!!!
+                Re:将棋盤 へようこそ！<br/>
+                <span className="shogi-yorokobi-submessage">この機能は実験的です。もし気になった点があれば、気軽にフィードバックをお願いします。</span>
             </div>
             <div className="shogi-type-selector">
-                <HistoryAnchor className="shogi-type-selector-button" aria-disabled="true">For who?</HistoryAnchor>
+                <HistoryAnchor className="shogi-type-selector-button" aria-disabled="true">For you(Coming soon)</HistoryAnchor>
                 <HistoryAnchor className="shogi-type-selector-button" data-is-active={location.pathname.replace(/\?.*/, "") === "/ranking"} href="/ranking">メイン</HistoryAnchor>
                 <HistoryAnchor className="shogi-type-selector-button" data-is-active={location.pathname.startsWith("/ranking/genre")} href="/ranking/genre">総合ランキング</HistoryAnchor>
             </div>
