e1b937602782af7f0a959943a2156943c98b56c9
castella-cake <65476384+castella-cake@users.noreply.github.com>
2025-04-19 16:40:39 +0900

    FIX: フォロー中の表示が別の動画に移動した際に正しく反映されない問題を修正

--- components/PMWatch/modules/Info/UserFollowButton.tsx
+++ components/PMWatch/modules/Info/UserFollowButton.tsx
@@ -2,38 +2,41 @@ import { UserFollowApiDataRootObject } from "@/types/UserFollowApiData";
 import { IconCheck, IconProgressHelp, IconStar} from "@tabler/icons-react";
 
 export default function UserFollowButton({ userId }: { userId: string | number }) {
-    const [isFollowing, setIsFollowing] = useState<boolean | null>(null);
+    const [isFollowingData, setIsFollowingData] = useState<{ following: boolean, userId: string | number} | null>(null);
     useEffect(() => {
         async function getFollowStatus() {
+            // 同じユーザーIDだったらfetchをスキップ(無駄なので)
+            if (isFollowingData && isFollowingData.userId === userId) return;
+
             const response: UserFollowApiDataRootObject = await userFollowApi(userId, "GET");
             if (response.data && response.data.following) {
-                setIsFollowing(true);
+                setIsFollowingData({ following: true, userId });
             } else {
-                setIsFollowing(false);
+                setIsFollowingData({ following: false, userId });
             }
         }
         getFollowStatus();
-    }, [])
+    }, [userId])
 
     async function onFollowClick() {
-        if (isFollowing) {
+        if (isFollowingData && isFollowingData.following) {
             const response: UserFollowApiDataRootObject = await userFollowApi(userId, "DELETE");
-            if ( response.meta.status === 200 ) setIsFollowing(false);
+            if ( response.meta.status === 200 ) setIsFollowingData({ following: false, userId });
         } else {
             const response: UserFollowApiDataRootObject = await userFollowApi(userId, "POST");
-            if ( response.meta.status === 200 ) setIsFollowing(true);
+            if ( response.meta.status === 200 ) setIsFollowingData({ following: true, userId });
         }
     }
 
-    if (isFollowing === null) return <button className="videoinfo-owner-followbtn">
+    if (isFollowingData === null) return <button className="videoinfo-owner-followbtn">
         <IconProgressHelp/>
         <br/>
         <span>ロード中</span>
     </button>
 
-    return <button className="videoinfo-owner-followbtn" title={isFollowing ? "フォロー解除" : "フォロー"} onClick={onFollowClick}>
-        {isFollowing ? <IconCheck/> : <IconStar/>}
+    return <button className="videoinfo-owner-followbtn" title={isFollowingData.following ? "フォロー解除" : "フォロー"} onClick={onFollowClick}>
+        {isFollowingData.following ? <IconCheck/> : <IconStar/>}
         <br/>
-        <span>{isFollowing ? "フォロー中" : "フォロー"}</span>
+        <span>{isFollowingData.following ? "フォロー中" : "フォロー"}</span>
     </button>
 }
