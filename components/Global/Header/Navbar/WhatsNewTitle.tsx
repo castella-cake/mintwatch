2ee9efb473a2faa8308e5bc7c78d4dd344ab6c10
castella-cake <65476384+castella-cake@users.noreply.github.com>
2025-04-21 22:39:49 +0900

    FIX: 更新情報を視聴ページ以外で表示しない

--- components/Global/Header/Navbar/WhatsNewTitle.tsx
+++ components/Global/Header/Navbar/WhatsNewTitle.tsx
@@ -1,10 +1,13 @@
 import whatsNewData from "@/assets/whatsnew.json"
 import { useSetVideoActionModalStateContext } from "../../Contexts/ModalStateProvider"
+import { useLocationContext } from "@/components/Router/RouterContext"
 export default function WhatsNewTitle() {
+    const location = useLocationContext()
     const setVideoActionModalState = useSetVideoActionModalStateContext()
     const onClick = useCallback(() => {
         setVideoActionModalState("whatsnew")
     }, [])
+    if (!location.pathname.startsWith("/watch")) return
     return <button className="whatsnew-container" onClick={onClick}>
         <div className="whatsnew-title">更新情報</div>
         <div className="whatsnew-body">{whatsNewData.title}</div>
