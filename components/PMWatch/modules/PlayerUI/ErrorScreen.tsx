300de2894f20251dd3060fe868ef5870bcc4b17b
castella-cake <65476384+castella-cake@users.noreply.github.com>
2025-04-28 17:49:38 +0900

    CHANGE: 再生不可画面を少し変更

--- components/PMWatch/modules/PlayerUI/ErrorScreen.tsx
+++ components/PMWatch/modules/PlayerUI/ErrorScreen.tsx
@@ -4,14 +4,18 @@ import DAnimeLinks from "./DAnimeLinks";
 export function ErrorScreen({ videoInfo }: { videoInfo: VideoDataRootObject | null}) {
     if (!videoInfo || videoInfo.data?.response.media.domand && videoInfo.data.response.viewer) return
     const isPPV = videoInfo.data?.response.payment.preview.ppv.isEnabled
+    const isAdmissionOnly = videoInfo.data?.response.payment.preview.admission.isEnabled
     const isPremiumOnly = videoInfo.data?.response.payment.preview.premium.isEnabled
     const viewer = videoInfo.data.response.viewer
     return <div className="player-errorscreen-wrapper"><div className="player-errorscreen">
-        <h2 className="player-errorscreen-title">{viewer ? "有料動画" : "ログインが必要です"}</h2>
+        <h2 className="player-errorscreen-title">{viewer ? "視聴できません" : "ログインが必要です"}</h2>
         <div className="player-errorscreen-body">
             {!viewer && <p>MintWatch はゲストアカウントでの視聴に対応していません。<br/>続けるにはログインしてください。</p>}
-            {isPPV && "未レンタル/未加入のため視聴できません。"}
-            {isPremiumOnly && "この動画は現在プレミアム限定動画です。プレミアム会員に加入することでも視聴できます。"}
+            <p>
+                {isPPV && <span>未レンタルのため視聴できません。</span>}
+                {isAdmissionOnly && <span>チャンネルに未加入のため視聴できません。</span>}
+            </p>
+            {isPremiumOnly && <p>この動画は現在プレミアム限定動画です。プレミアム会員に加入することでも視聴できます。</p>}
         </div>
         <DAnimeLinks/>
     </div></div>
