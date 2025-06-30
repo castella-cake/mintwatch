665a48b277ef73a554b3a4e5902f652b07c2e12b
castella-cake <65476384+castella-cake@users.noreply.github.com>
2025-07-01 03:10:20 +0900

    FEAT: マイリストへの追加時にアラートを表示 (#32)

--- components/PMWatch/modules/videoAction/MylistUI.tsx
+++ components/PMWatch/modules/videoAction/MylistUI.tsx
@@ -1,31 +1,36 @@
-import { Dispatch, SetStateAction, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { MylistsResponseRootObject } from "@/types/mylistsData";
 import { VideoDataRootObject } from "@/types/VideoData";
 import { IconCheck } from "@tabler/icons-react";
+import { useSetAlertContext } from "@/components/Global/Contexts/AlertProvider";
 
 type Props = {
     onClose: () => void,
     videoInfo: VideoDataRootObject,
 }
 
-async function onAddToMylist(mylistId: number, itemId: string, setAddedMylists: Dispatch<SetStateAction<number[]>>) {
-    const response = await addItemToMylist(mylistId, itemId, location.href)
-    if (response.meta.status === 201) {
-        setAddedMylists(( current ) => [ ...current, mylistId ])
-        return true
-    }
-}
-
 export function Mylist({ onClose, videoInfo }: Props) {
+    const { showAlert } = useSetAlertContext();
+
     const [ mylistsData, setMylistsData ] = useState<MylistsResponseRootObject | null>(null);
     const [ addedMylists, setAddedMylists ] = useState<number[]>([]);
     useEffect(() => {
         async function getData() {
             const response: MylistsResponseRootObject = await getMylists()
-            if (response.meta.status === 200) setMylistsData(response)
+            if (response.meta.status === 200) {
+                setMylistsData(response)
+            }
         }
         getData()
     }, [])
+    async function onAddToMylist(mylistId: number, itemId: string) {
+        const response = await addItemToMylist(mylistId, itemId, location.href)
+        if (response.meta.status === 201) {
+            setAddedMylists(( current ) => [ ...current, mylistId ])
+            showAlert({ title: "マイリストに追加しました", icon: <IconCheck/> })
+            return true
+        }
+    }
     const videoTitle = (videoInfo.data.response && videoInfo.data.response.video && videoInfo.data.response.video.title) ? videoInfo.data.response.video.title : "タイトル不明"
     return <div className="mylists-container" id="pmw-mylists">
         <div className="mylists-title videoaction-actiontitle">
@@ -39,7 +44,7 @@ export function Mylist({ onClose, videoInfo }: Props) {
             {
                 mylistsData ? mylistsData.data.mylists.map(elem => {
                     return <button key={ elem.id } className="mylist-item" onClick={() => {
-                        if ( !addedMylists.includes(elem.id) && videoInfo.data ) onAddToMylist(elem.id, videoInfo.data.response.video.id, setAddedMylists)
+                        if ( !addedMylists.includes(elem.id) && videoInfo.data ) onAddToMylist(elem.id, videoInfo.data.response.video.id)
                     }}>
                         {addedMylists.includes(elem.id) && <><IconCheck/>追加済み: </>}
                         <span className="mylist-title">{ elem.name }</span><br/>
