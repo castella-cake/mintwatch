9d9113055bdd2d4fcb6d245104e882487d6819df
castella-cake <65476384+castella-cake@users.noreply.github.com>
2025-07-01 03:30:24 +0900

    FIX: 不要になったデバッグボタンを削除

--- components/Global/PluginList.tsx
+++ components/Global/PluginList.tsx
@@ -1,30 +1,8 @@
-import { IconAlertTriangle, IconChevronDown, IconChevronUp } from "@tabler/icons-react";
+import { IconChevronDown, IconChevronUp } from "@tabler/icons-react";
 import { useState } from "react";
-import { useSetAlertContext } from "./Contexts/AlertProvider";
 
 function PluginList() {
-    const { showAlert } = useSetAlertContext()
     const [ isPluginListShown, setIsPluginListShown ] = useState(false)
-
-    const onTestOpen = () => {
-        showAlert({
-            title: "ただいまテスト中",
-            body: "テスト用のダイアログです。\nこの操作は元に戻せます。続行しますか？",
-            icon: <IconAlertTriangle/>,
-            customCloseButton: [
-                { text: "キャンセル", key: "cancel" },
-                { text: "わかった", key: "ok" },
-            ],
-            onClose: (key) => {
-                if (key === "ok") {
-                    showAlert({
-                        title: "わかった",
-                        body: "ごいけん　ありがとうございます！\nそれでは　また！",
-                    })
-                }
-            }
-        })
-    }
 
     return <footer className="pmw-footer" id="pmw-footer">
         <div className="footer-items global-flex">
@@ -35,7 +13,6 @@ function PluginList() {
                 <a href="https://github.com/sponsors/castella-cake" target="_blank" rel="noopener noreferrer" className="titlelink">Sponsor</a>
                 <a href="https://github.com/castella-cake/niconico-peppermint-extension/issues" target="_blank" rel="noopener noreferrer" className="titlelink">Feedback</a>
                 <button className="footer-pagetop-button" onClick={() => {window.scroll({ top: 0, behavior: "smooth" })}}><IconChevronUp/><span>PAGE TOP</span></button>
-                <button className="footer-pagetop-button" onClick={onTestOpen}><IconChevronUp/><span>OPEN</span></button>
             </div>
         </div>
         <div className={isPluginListShown ? "plugin-list-display" : "plugin-list-display-hidden"}>
