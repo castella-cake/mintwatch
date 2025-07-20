efe6172e1c9fc864d7b09f7f3720e427e2cb1cb4
castella-cake <65476384+castella-cake@users.noreply.github.com>
2025-07-20 20:58:37 +0900

    FEAT: ESLint と ESLint Stylistic を追加 (#40 進行)

---
+++ eslint.config.js
@@ -0,0 +1,45 @@
+import js from "@eslint/js"
+import globals from "globals"
+import tseslint from "typescript-eslint"
+import pluginReact from "eslint-plugin-react"
+import { defineConfig, globalIgnores } from "eslint/config"
+import autoImports from "./.wxt/eslint-auto-imports.mjs"
+import stylistic from "@stylistic/eslint-plugin"
+
+export default defineConfig([
+    autoImports,
+    {
+        files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+        plugins: {
+            js,
+            '@stylistic': stylistic
+        },
+        extends: ["js/recommended"],
+    },
+    {
+        files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+        languageOptions: { globals: globals.browser },
+    },
+    stylistic.configs.customize({
+        indent: 4,
+        quotes: "double",
+        semi: false,
+    }),
+    tseslint.configs.recommended,
+    pluginReact.configs.flat.recommended,
+    pluginReact.configs.flat["jsx-runtime"],
+    globalIgnores(["node_modules/*", ".output/*", ".wxt/*"]),
+    {
+        settings: {
+            react: {
+                version: "detect",
+            },
+        },
+        rules: {
+            "@typescript-eslint/no-explicit-any": "off",
+            "react/jsx-no-undef": ["error", {
+                allowGlobals: true,
+            }]
+        },
+    },
+])
