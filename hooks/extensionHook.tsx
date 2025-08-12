f6bc66fdde0117dee9a72378dc617a79aa085a0f
castella-cake <65476384+castella-cake@users.noreply.github.com>
2025-08-12 20:59:09 +0900

    FEAT: StorageVarManager を実装して共通キャッシュを持たせる

--- hooks/extensionHook.tsx
+++ hooks/extensionHook.tsx
@@ -1,4 +1,5 @@
-import { useState, useEffect, useContext, createContext, ReactNode, useSyncExternalStore } from "react"
+import { useState, useEffect, useContext, createContext, ReactNode, useSyncExternalStore, RefObject } from "react"
+import { Unwatch, WatchCallback } from "wxt/utils/storage"
 
 async function getSyncStorageData() {
     return await browser.storage.sync.get(null)
@@ -90,18 +91,36 @@ export function useStorage() {
     return { storages, setLocalStorageValue, setSyncStorageValue }
 }
 
-const IStorageContext = createContext<{
-    syncStorage: { [key: string]: any }
-    setSyncStorageValue: (name: string, value: any, silent?: boolean) => void
-    localStorage: { [key: string]: any }
-    setLocalStorageValue: (name: string, value: any, silent?: boolean) => void
-    isLoaded: boolean
-}>({ syncStorage: {}, setSyncStorageValue: () => {}, localStorage: {}, setLocalStorageValue: () => {}, isLoaded: false })
+type StorageVarManager = {
+    watch: (key: StorageItemKey, callback: WatchCallback<unknown>) => () => void
+    getItems: <K extends readonly StorageItemKey[]>(keys: K) => Promise<{ [P in K[number]]?: any }>
+    _cachedStorage: Map<StorageItemKey, any>
+}
+
+const IStorageContext = createContext<RefObject<StorageVarManager> | null>(null)
 
 export function StorageProvider({ children }: { children: ReactNode }) {
-    const { storages, setLocalStorageValue, setSyncStorageValue } = useStorage()
+    const storageVarRef = useRef(storageVar())
+    useEffect(() => {
+        storage.snapshot("sync").then((snapshot) => {
+            const prefixedSnapshot = Object.keys(snapshot).map((key) => {
+                return { [`sync:${key}`]: snapshot[key as keyof typeof snapshot] }
+            }).reduce((p, c) => Object.assign(p, c), {})
+            for (const key in prefixedSnapshot) {
+                storageVarRef.current._cachedStorage.set(key as StorageItemKey, prefixedSnapshot[key as keyof typeof prefixedSnapshot])
+            }
+        })
+        storage.snapshot("local").then((snapshot) => {
+            const prefixedSnapshot = Object.keys(snapshot).map((key) => {
+                return { [`local:${key}`]: snapshot[key as keyof typeof snapshot] }
+            }).reduce((p, c) => Object.assign(p, c), {})
+            for (const key in prefixedSnapshot) {
+                storageVarRef.current._cachedStorage.set(key as StorageItemKey, prefixedSnapshot[key as keyof typeof prefixedSnapshot])
+            }
+        })
+    }, [])
     return (
-        <IStorageContext value={{ syncStorage: storages.sync, setSyncStorageValue, localStorage: storages.local, setLocalStorageValue, isLoaded: storages.isLoaded }}>
+        <IStorageContext value={storageVarRef}>
             {children}
         </IStorageContext>
     )
@@ -111,6 +130,50 @@ export function useStorageContext() {
     return useContext(IStorageContext)
 }
 
+export function storageVar(): StorageVarManager {
+    const watch = new Map<StorageItemKey, Unwatch>()
+    const callbacks = new Map<StorageItemKey, Map<string, WatchCallback<unknown>>>()
+    const cachedStorage = new Map<StorageItemKey, any>()
+
+    return {
+        watch: (key: StorageItemKey, callback: WatchCallback<unknown>) => {
+            const subscribeKey = crypto.randomUUID()
+            if (callbacks.has(key)) {
+                callbacks.get(key)!.set(subscribeKey, callback)
+            } else {
+                callbacks.set(key, new Map().set(subscribeKey, callback))
+            }
+            if (!watch.has(key)) {
+                watch.set(key, storage.watch(key, (newValue, oldValue) => {
+                    callbacks.get(key)!.forEach((callback) => {
+                        callback(newValue, oldValue)
+                    })
+                    cachedStorage.set(key, newValue)
+                }))
+            }
+            return () => {
+                if (callbacks.has(key)) {
+                    callbacks.get(key)!.delete(subscribeKey)
+                }
+            }
+        },
+        getItems: async <K extends readonly StorageItemKey[]>(keys: K): Promise<{ [P in K[number]]?: any }> => {
+            const object = {}
+            for (const key of keys) {
+                if (cachedStorage.get(key) !== undefined) {
+                    Object.assign(object, { [key]: cachedStorage.get(key) })
+                } else {
+                    const currentValue = await storage.getItem(key)
+                    Object.assign(object, { [key]: currentValue })
+                    cachedStorage.set(key, currentValue)
+                }
+            }
+            return object
+        },
+        _cachedStorage: cachedStorage,
+    }
+}
+
 /**
  * 拡張機能ストレージから必要なキーのみを取得するフック
  * @param keys 取得するキーの配列(as const を使うこと)
@@ -118,12 +181,14 @@ export function useStorageContext() {
  * @returns 統合されたオブジェクト
  */
 export function useStorageVar<K extends readonly string[]>(keys: K, type: "sync" | "local" | "session" | "managed" = "sync"): { [P in K[number]]?: any } {
+    const storageManager = useStorageContext()
     const _storageRef = useRef<{ [P in K[number]]: any }>({} as { [P in K[number]]: any })
     const subscribe = useCallback((onUpdate: () => void) => {
         let aborted = false
         // StorageItemKey の形に直して取得
-        getStorageItemsWithObject(keys.map(key => `${type}:${key}` as StorageItemKey)).then((object) => {
+        storageManager?.current.getItems(keys.map(key => `${type}:${key}` as StorageItemKey)).then((object) => {
             if (!aborted) {
+                // keyからtypeを取り除く
                 _storageRef.current = Object.keys(object).map((key) => {
                     return { [key.replace(`${type}:`, "")]: object[key as keyof typeof object] } as { [P in K[number]]: string }
                 }).reduce((p, c) => Object.assign(p, c), {} as { [P in K[number]]: string })
@@ -131,7 +196,7 @@ export function useStorageVar<K extends readonly string[]>(keys: K, type: "sync"
             }
         })
         // watch は unwatch の関数を返す
-        const unwatchFunctions = keys.map(key => storage.watch(`${type}:${key}` as StorageItemKey, (n) => {
+        const unwatchFunctions = keys.map(key => storageManager?.current.watch(`${type}:${key}` as StorageItemKey, (n) => {
             if (!aborted) {
                 _storageRef.current = { ..._storageRef.current, [key]: n } as { [P in K[number]]: string }
                 onUpdate()
@@ -140,10 +205,10 @@ export function useStorageVar<K extends readonly string[]>(keys: K, type: "sync"
         return () => {
             aborted = true
             for (const unwatch of unwatchFunctions) {
-                unwatch()
+                if (unwatch) unwatch()
             }
         }
-    }, [])
+    }, [storageManager])
     const storageObject = useSyncExternalStore(subscribe, () => _storageRef.current)
     return storageObject
 }
