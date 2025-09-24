6dc39cb18223d21d83adf707edc8913ea5bfe4f3
castella-cake <65476384+castella-cake@users.noreply.github.com>
2025-09-25 05:46:25 +0900

    FIX: ユーザー検索のgridを仮修正

--- components/Search/GenericComponents/UserItemCard.tsx
+++ components/Search/GenericComponents/UserItemCard.tsx
@@ -5,31 +5,34 @@ export function UserItemCard({ user, markAsLazy }: { user: SearchUserItem, markA
     return (
         <a className="useritem-wrapper" key={user.id} href={userIdToUserUrl(user.id.toString())}>
             <img className="useritem-icon" src={user.icons.large} loading={markAsLazy ? "lazy" : undefined} />
-            <div className="useritem-nickname">
-                {user.nickname}
+            <div className="useritem-datacolumn">
+                <div className="useritem-nickname">
+                    {user.nickname}
+                </div>
+                <div className="useritem-strippeddesc">
+                    {user.strippedDescription}
+                </div>
+                <div className="useritem-count">
+                    <span>
+                        フォロワー
+                        {" "}
+                        <strong>{user.followerCount}</strong>
+                    </span>
+                    <span>
+                        動画
+                        {" "}
+                        <strong>{user.videoCount}</strong>
+                    </span>
+                    <span>
+                        番組
+                        {" "}
+                        <strong>
+                            {user.liveCount}
+                        </strong>
+                    </span>
+                </div>
             </div>
-            <div className="useritem-strippeddesc">
-                {user.strippedDescription}
-            </div>
-            <div className="useritem-count">
-                <span>
-                    フォロワー
-                    {" "}
-                    <strong>{user.followerCount}</strong>
-                </span>
-                <span>
-                    動画
-                    {" "}
-                    <strong>{user.videoCount}</strong>
-                </span>
-                <span>
-                    番組
-                    {" "}
-                    <strong>
-                        {user.liveCount}
-                    </strong>
-                </span>
-            </div>
+
         </a>
     )
 }
