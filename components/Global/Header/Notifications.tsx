2673a24dc5397c56f70cceeb67922684ca50284d
castella-cake <65476384+castella-cake@users.noreply.github.com>
2025-12-01 23:49:12 +0900

    FEAT: お知らせボックスに各ページへのリンクを追加

--- components/Global/Header/Notifications.tsx
+++ components/Global/Header/Notifications.tsx
@@ -1,5 +1,6 @@
 import { useOshiraseBoxData } from "@/hooks/apiHooks/global/oshiraseBox"
 import { NotificationCard } from "./NotificationCard"
+import { IconExternalLink, IconSettings } from "@tabler/icons-react"
 
 function onNotificationClick(id: string, isRead: boolean) {
     if (isRead) return
@@ -7,13 +8,32 @@ function onNotificationClick(id: string, isRead: boolean) {
 }
 
 export default function Notifications() {
-    const oshiraseBoxData = useOshiraseBoxData()
+    const { oshiraseBoxData } = useOshiraseBoxData()
     return (
         <div className="notifications-container" id="pmw-notifications">
             <div className="header-action-title">
                 <div className="header-action-title-left"></div>
                 <div className="header-action-title-center">通知</div>
-                <div className="header-action-title-right"></div>
+                <div className="header-action-title-right">
+                    <a
+                        href="https://inform.nicovideo.jp/oshirase/settings"
+                        className="header-action-title-button"
+                        target="_blank"
+                        rel="noreferrer"
+                        title="お知らせ設定を開く (別タブで開きます)"
+                    >
+                        <IconSettings />
+                    </a>
+                    <a
+                        href="https://inform.nicovideo.jp/oshirase/list"
+                        className="header-action-title-button"
+                        target="_blank"
+                        rel="noreferrer"
+                        title="お知らせ一覧を開く (別タブで開きます)"
+                    >
+                        <IconExternalLink />
+                    </a>
+                </div>
             </div>
             <div className="notifications-content">
                 {
