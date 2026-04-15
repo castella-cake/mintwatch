a43bd378e4d98979423fbc7971789c8fe0cba0d7
castella-cake <65476384+castella-cake@users.noreply.github.com>
2026-04-15 22:09:36 +0900

    FIX: ssから始まる動画IDを検出可能にする

--- utils/detectVideoId.ts
+++ utils/detectVideoId.ts
@@ -1,4 +1,4 @@
-const videoIdRegex = /(?:s[mo]|n[lm])(?!0)\d+/g
+const videoIdRegex = /(?:s[mos]|n[lm])(?!0)\d+/g
 export function detectVideoIdFromString(string: string): string[] | null {
     const matches = string.match(videoIdRegex)
     return matches ? Array.from(new Set(matches)) : null
