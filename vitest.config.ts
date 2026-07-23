eac38b8a47ef38e370a8d1b3072a7a1dcd686134
castella-cake <65476384+castella-cake@users.noreply.github.com>
2026-07-24 06:35:42 +0900

    FEAT: Vitestによるユニットテストを構成する

---
+++ vitest.config.ts
@@ -0,0 +1,11 @@
+import { configDefaults, defineConfig } from "vitest/config"
+import { WxtVitest } from "wxt/testing/vitest-plugin"
+
+export default defineConfig({
+    plugins: [WxtVitest()],
+    test: { exclude: [
+        ...configDefaults.exclude,
+        "**/e2e/**",
+        "**/*.spec.ts",
+    ] },
+})
