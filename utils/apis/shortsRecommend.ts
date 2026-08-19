398940326f386ef54d987661cc8d72ca7a76bfa8
castella-cake <65476384+castella-cake@users.noreply.github.com>
2026-08-20 04:56:56 +0900

    FEAT: ショートキューに必要なAPIを実装

---
+++ utils/apis/shortsRecommend.ts
@@ -0,0 +1,31 @@
+import APIError from "../classes/APIError"
+import { PlaylistRecipeIdDataRootObject } from "@/types/PlaylistRecipeIdData"
+
+export type ShortsRecommendOptions = {
+    recipeId?: string
+    recipeVersion?: number
+    limit?: number
+}
+
+export async function getShortsRecommend(videoId: string, options: ShortsRecommendOptions = {}) {
+    const params = new URLSearchParams({
+        recipeId: options.recipeId ?? "video_short_watch_recommendation",
+        recipeVersion: String(options.recipeVersion ?? 1),
+        site: "nicovideo",
+        videoId,
+        currentVideoId: videoId,
+        limit: String(options.limit ?? 25),
+    })
+    const response = await fetch(`https://nvapi.nicovideo.jp/v1/playlist/recipe-id?${params.toString()}`, {
+        credentials: "include",
+        method: "GET",
+        headers: {
+            "x-frontend-id": "6",
+            "x-frontend-version": "0",
+            "x-niconico-language": "ja-jp",
+        },
+    })
+    const responseJson: PlaylistRecipeIdDataRootObject = await response.json()
+    if (!validateBaseResponse(responseJson)) throw new APIError("Shorts recommend API failed.", responseJson)
+    return responseJson
+}
