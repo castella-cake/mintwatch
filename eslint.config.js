c1b8324473ab1bee43eac775a444971d2cacb565
castella-cake <65476384+castella-cake@users.noreply.github.com>
2026-09-13 23:48:25 +0900

    FIX: TypeScript-ESLintが未使用変数で重複エラーを起こすのでTypeScript側のみにする

--- eslint.config.js
+++ eslint.config.js
@@ -38,6 +38,7 @@ export default defineConfig([
         },
         rules: {
             "@typescript-eslint/no-explicit-any": "off",
+            "@typescript-eslint/no-unused-vars": "off", // typescript側で見る
             "react/jsx-no-undef": ["error", {
                 allowGlobals: true,
             }],
